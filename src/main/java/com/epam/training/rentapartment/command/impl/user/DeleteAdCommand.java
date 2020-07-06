eb25e4355fc91c16787952d157c83d3887c53414
Your Name <you@example.com>
2020-07-06 09:04:54 +0300

    new classes

---
+++ src/main/java/com/epam/training/rentapartment/command/impl/user/DeleteAdCommand.java
@@ -0,0 +1,12 @@
+package com.epam.training.rentapartment.command.impl.user;
+
+import com.epam.training.rentapartment.command.Command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class DeleteAdCommand implements Command {
+    @Override
+    public String execute(HttpServletRequest request) {
+        return null;
+    }
+}
---
+++ src/main/java/com/epam/training/rentapartment/service/UserService.java
@@ -0,0 +1,18 @@
+package com.epam.training.rentapartment.service;
+
+import com.epam.training.rentapartment.repository.impl.user.UserRepository;
+
+public class UserService { //TODO
+    private UserRepository userRepository;
+
+    public void logOut() {
+    }
+
+    public void addAd() {
+    }
+
+    public void deleteAd() {
+    }
+
+
+}
