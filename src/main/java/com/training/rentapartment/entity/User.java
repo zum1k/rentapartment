662ad4f9143d85335d2583ad4b5bce18793e043e
Your Name <you@example.com>
2020-07-13 09:40:32 +0300

    -constant fields in User

--- src/main/java/com/training/rentapartment/entity/User.java
+++ src/main/java/com/training/rentapartment/entity/User.java
@@ -3,20 +3,6 @@ package com.training.rentapartment.entity;
 import java.util.Objects;
 
 public class User {
-    //SQL table and columns names
-    public static final String USER_TABLE_NAME = "users";
-    public static final String LOGIN = "login";
-    public static final String PASSWORD = "password";
-    public static final String EMAIL = "email";
-    public static final String USER_ID = "user_id";
-    public static final String TYPE = "type";
-    //SQL preparedStatement indexes of columns
-    public static final int ID_INDEX = 1;
-    public static final int LOGIN_INDEX = 2;
-    public static final int PASSWORD_INDEX = 3;
-    public static final int EMAIL_INDEX = 4;
-    public static final int TYPE_INDEX = 5;
-    //class fields
     private long userId;
     private String login;
     private String password;
